5791cc2800c61161ac5a74dffb96bda64492bd9c
Mohammad.R Ashtari <125213272+Mrezapbs@users.noreply.github.com>
2024-07-24 09:05:11 +0330

    Update StepMotor.h

--- StepMotor.h
+++ StepMotor.h
@@ -13,43 +13,43 @@
 typedef enum
 {
 	DEFAULT_TOTAL_NUM_OF_STEPS	= 900,
-
-	START_UP					= 1,
-	FIND_REFERENCE				= 2,
-	NORMAL_OPERATION			= 3,
-	SHUT_DOWN_OPERATION			= 4,
-
-	COIL1_EXCITED	            = 11,
-    COIL1_2_EXCITED             = 12,
-    COIL2_EXCITED               = 13,
-    COIL2_3_EXCITED             = 14,
-    COIL3_EXCITED               = 15,
-    COIL3_4_EXCITED             = 16,
-    COIL4_EXCITED               = 17,
-    COIL4_1_EXCITED             = 18,
-	START_UP_STAGE	            = 19,
-
-	STEP_MOTOR_MIN_POS			= 0,
-	STEP_MOTOR_MAX_POS			= 100,
-	CLOSE_160_PERCENT			= -60,
-
+	
+	START_UP			= 1,
+	FIND_REFERENCE			= 2,
+	NORMAL_OPERATION		= 3,
+	SHUT_DOWN_OPERATION		= 4,
+	
+	COIL1_EXCITED	        	= 11,
+	COIL1_2_EXCITED             	= 12,
+	COIL2_EXCITED               	= 13,
+	COIL2_3_EXCITED             	= 14,
+	COIL3_EXCITED               	= 15,
+	COIL3_4_EXCITED             	= 16,
+	COIL4_EXCITED               	= 17,
+	COIL4_1_EXCITED             	= 18,
+	START_UP_STAGE	            	= 19,
+	
+	STEP_MOTOR_MIN_POS		= 0,
+	STEP_MOTOR_MAX_POS		= 100,
+	CLOSE_160_PERCENT		= -60,
+	
 	SWING_MIN_POS_PERCENT		= 33,
 	SWING_MAX_POS_PERCENT		= 83,
-
-	OPEN_VENT					= 1,
-	CLOSE_VENT					= 2,
-
-	FLAP_STATE_STEP_1            = 1,
-	FLAP_STATE_STEP_2            = 2,
-	FLAP_STATE_STEP_3            = 3,
-	FLAP_STATE_STEP_4            = 4,
-	FLAP_STATE_STEP_5            = 5,
-
-	FLAP_STATE_POS_1_PERCENT    = 33,
-	FLAP_STATE_POS_2_PERCENT    = 44,
-	FLAP_STATE_POS_3_PERCENT    = 58,
-	FLAP_STATE_POS_4_PERCENT    = 72,
-	FLAP_STATE_POS_5_PERCENT    = 95,
+	
+	OPEN_VENT			= 1,
+	CLOSE_VENT			= 2,
+	
+	FLAP_STATE_STEP_1            	= 1,
+	FLAP_STATE_STEP_2            	= 2,
+	FLAP_STATE_STEP_3            	= 3,
+	FLAP_STATE_STEP_4            	= 4,
+	FLAP_STATE_STEP_5            	= 5,
+	
+	FLAP_STATE_POS_1_PERCENT    	= 33,
+	FLAP_STATE_POS_2_PERCENT    	= 44,
+	FLAP_STATE_POS_3_PERCENT    	= 58,
+	FLAP_STATE_POS_4_PERCENT    	= 72,
+	FLAP_STATE_POS_5_PERCENT    	= 95,
 
 
 }StepMotorConstants;
